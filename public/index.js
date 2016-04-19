543482e093a4e6be9cddc0a0b739e1f58ea9b491
Didier Franc <mail@didierfranc.com>
2016-04-19 18:14:34 +0200

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,2 +1,10 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+
+var HelloMessage = React.createClass({
+  render: function() {
+    return <div>Hello {this.props.name}</div>
+  }
+})
+
+ReactDOM.render(<HelloMessage name="Mark" />, document.getElementbyId('content'))
